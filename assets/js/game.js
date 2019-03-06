663ee8d18db8c3ebb72a05c48c1d0c60a91b298f
treyarnold <arnold.trey80@gmail.com>
2019-03-05 21:58:35 -0600

    working on js, but broken

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,64 @@
 let words = ["Dumbledore", "Muggle", "Squib", "Basilisk", "Hippogriff", "Nagini", "Mandrake", "Bellatrix", "Draco", "Arithmancy", "Transfiguration", "Obliviate", "Riddikulus", "Sectumsempra", "Avada Kedavra", "Alohomora", "Lumos", "Expelliarmus", "Wingardium Leviosa", "Accio", "Expecto Patronum"];
+let currentWord = "";
+let numGuesses = 0;
+let wins = 0;
+let losses = 0;
+let guesses = [];
+let blanks = "";
+const input = document.querySelector('body');
+
+input.addEventListener('keydown', checkLetter);
 
 function randNum (max) {
      return Math.floor(Math.random() * Math.floor(max));
 }
 
-let currentWord = "";
-let guesses = 0;
-let wins = 0;
-let losses = 0;
-
 function reset () {
-     currentWord = words[randNum(words.length)];
-     guesses = 10;
+     let lastWord = currentWord;
+     while (lastWord == currentWord) {
+          currentWord = words[randNum(words.length)];
+     }
+     numGuesses = 10;
+     for (var i = 0; i < currentWord.length; i++) {
+          if (currentWord[i] == " ") {
+               blanks += " / ";
+          }
+          else {
+               blanks += "_ ";
+          }
+     }
+     document.getElementById("blanks").textContent = blanks;
+     document.getElementById("result").textContent = currentWord;
 }
+
+function correct (letter) {
+     for (var i = 0; i < currentWord.length; i++) {
+          if (currentWord[i].toLowerCase() == letter) {
+               blanks += letter + " ";
+          } else if (currentWord[i] == " ") {
+               blanks += " / ";
+          } else {
+               blanks += "_ ";
+          }
+     }
+     guesses.push(letter);
+     document.getElementById("blanks").textContent = blanks;
+     if (blanks.includes("_")) {
+
+     } else {
+          winner();
+     }
+}
+
+function checkLetter(event) {
+     let letter = event.key;
+     if (!(letter in guesses)) {
+          if (currentWord.includes(letter)) {
+               correct(letter);
+          } else {
+               wrong(letter);
+          }
+     }
+}
+
+reset();
