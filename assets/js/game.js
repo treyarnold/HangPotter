3d58dfb52f35eae14e803f77fdd50cb4560ef66a
treyarnold <arnold.trey80@gmail.com>
2019-03-06 14:56:25 -0600

    basic game complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 let words = ["Dumbledore", "Muggle", "Squib", "Basilisk", "Hippogriff", "Nagini", "Mandrake", "Bellatrix", "Draco", "Arithmancy", "Transfiguration", "Obliviate", "Riddikulus", "Sectumsempra", "Avada Kedavra", "Alohomora", "Lumos", "Expelliarmus", "Wingardium Leviosa", "Accio", "Expecto Patronum"];
 let currentWord = "";
-let numGuesses = 0;
+let numGuesses = 10;
 let wins = 0;
 let losses = 0;
 let guesses = [];
@@ -9,12 +9,22 @@ const input = document.querySelector('body');
 
 input.addEventListener('keydown', checkLetter);
 
+function isAlphaCharacter(letter) {
+     let check = letter.toLowerCase();
+     if ((check <= "z") && (check >= "a")) {
+          return true;
+     }
+     return false;
+}
+
 function randNum (max) {
      return Math.floor(Math.random() * Math.floor(max));
 }
 
 function reset () {
      let lastWord = currentWord;
+     blanks = "";
+     guesses = [];
      while (lastWord == currentWord) {
           currentWord = words[randNum(words.length)];
      }
@@ -27,37 +37,74 @@ function reset () {
                blanks += "_";
           }
      }
+     document.getElementById("guesses").textContent = numGuesses;
      document.getElementById("blanks").textContent = blanks;
-     document.getElementById("result").textContent = currentWord;
+     document.getElementById("guessed").textContent = guesses;
+     // document.getElementById("result").textContent = currentWord;
+}
+
+function winner () {
+     let result = document.getElementById("result")
+     result.style.color = "green";
+     result.textContent = "You got it!";
+     wins += 1;
+     document.getElementById("wins").textContent = wins;
+     reset();
+}
+
+function loser () {
+     let result = document.getElementById("result")
+     result.style.color = "red";
+     result.textContent = "You're a Muggle";
+     losses += 1;
+     document.getElementById("losses").textContent = losses;
+     reset();
 }
 
 function correct (letter) {
-     newBlanks = "";
+     let newBlanks = "";
      for (var i = 0; i < currentWord.length; i++) {
-          if (currentWord[i].toLowerCase() == letter) {
-               NewBlanks += letter;
-          } else if (currentWord[i] == " ") {
-               NewBlanks += " / ";
+          if ((isAlphaCharacter(blanks[i])) || (blanks[i] == "/")) {
+               newBlanks += blanks[i];
           } else {
-               NewBlanks += "_";
+               if (currentWord[i].toLowerCase() == letter) {
+                    if ((i == 0) || (newBlanks[i-1] == "/")) {
+                         newBlanks += letter.toUpperCase();
+                    } else {
+                         newBlanks += letter;
+                    }
+               } else {
+                    newBlanks += "_";
+               }
           }
      }
-     guesses.push(letter);
+     blanks = newBlanks;
      document.getElementById("blanks").textContent = blanks;
-     if (blanks.includes("_")) {
-
-     } else {
+     if (!(blanks.includes("_"))) {
           winner();
+     }
+}
+
+function wrong (letter) {
+     numGuesses -= 1;
+     document.getElementById("guesses").textContent = numGuesses;
+     if (numGuesses == 0) {
+          loser ();
      }
 }
 
 function checkLetter(event) {
      let letter = event.key;
-     if (!(guesses.includes(letter.toLowerCase()))) {
-          if (currentWord.includes(letter)) {
-               correct(letter);
-          } else {
-               wrong(letter);
+     let lowerWord = currentWord.toLowerCase();
+     if ((isAlphaCharacter(letter) && (letter.length == 1))) {
+          if (!(guesses.includes(letter.toLowerCase()))) {
+               guesses.push(letter);
+               document.getElementById("guessed").textContent = guesses;
+               if (lowerWord.includes(letter)) {
+                    correct(letter);
+               } else {
+                    wrong(letter);
+               }
           }
      }
 }
